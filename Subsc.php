76f974db45966cddbca483be46588719ff46d8b8
Nolan Wira <njw1389@rit.edu>
2024-04-14 11:03:01 -0400

    Recent

---
+++ Subsc.php
@@ -0,0 +1,2 @@
+Thank you <?php echo htmlspecialchars($_POST["name"]); ?> 
+for subscribing to our Newletter we will email <?php echo htmlspecialchars($_POST["email"]); ?> from now on with our newest articles.
